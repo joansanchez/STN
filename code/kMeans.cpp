812241a9ca180bc6ba30fbd8bf0d76530506a23b
Joan Linux Port <jsanchezgarcia13@gmail.com>
2018-11-10 18:20:58 +0000

    k-means algorithm working

--- code/kMeans.cpp
+++ code/kMeans.cpp
@@ -2,14 +2,31 @@
 
 void kMeans::calcKMeans(map<string,Member*> membersInfo, int k){
   calcFirstCenters(membersInfo, k);
-  printColors();
+  int ite = 0;
+  while (!estable){
+    cout << "Iteration: " << ite << endl;
+    ++ite;
+    findNearestCenter(membersInfo);
+    paintCenter();
+    recalcCenter();
+  }
+  printColorMatrix();
 }
 
 void kMeans::printColorMatrix(){
-
+  cout << "Writing on file NodeColors.txt" << endl;
+  ofstream myfile;
+  myfile.open ("outputs/NodeColors.txt");
+  map <string, string>::iterator it;
+  for (it = colors.begin(); it != colors.end(); ++it){
+    myfile << it->first << " " << it->second << endl;
+  }
+  myfile.close();
+  cout << "Writing on file NodeColors.txt completed" << endl;
 }
 
 void kMeans::calcFirstCenters(map<string,Member*> membersInfo, int k){
+  estable = false;
   map<string,Member*>::iterator it;
   for (it=membersInfo.begin(); it!= membersInfo.end(); ++it){
     mapKeys.push_back(it->first);
@@ -21,18 +38,61 @@ void kMeans::calcFirstCenters(map<string,Member*> membersInfo, int k){
   for (int i = 0; i<k; ++i){
     string tmp = mapKeys[distr(eng)];
     centers[i] = membersInfo[tmp]->getPoints();
+    stack<pair<int,int>> stackTmp;
+    newCenterCalc[i] = stackTmp;
     colors[tmp] = colCell[i];
+    centerKeys.push_back(tmp);
   }
 }
 
-void kMeans::printColors(){
-  cout << "Writing on file NodeColors.txt" << endl;
-  ofstream myfile;
-  myfile.open ("outputs/NodeColors.txt");
-  map <string, string>::iterator it;
-  for (it = colors.begin(); it != colors.end(); ++it){
-    myfile << it->first << " " << it->second << endl;
+void kMeans::findNearestCenter(map<string,Member*> membersInfo){
+  map<string,Member*>::iterator it;
+  for (it = membersInfo.begin(); it != membersInfo.end(); ++it){
+    int currentCentre;
+    float currentDistance = 100000.0;
+    map <int, pair<int, int>>::iterator itCenters;
+    for(itCenters = centers.begin(); itCenters != centers.end(); ++itCenters){
+      float tmpDist = calcDist(itCenters->second, it->second->getPoints());
+      if (tmpDist < currentDistance){
+        currentCentre = itCenters->first;
+        currentDistance = tmpDist;
+      }
+    }
+    colors[it->first] = colCell[currentCentre];
+    newCenterCalc[currentCentre].push(it->second->getPoints());
   }
-  myfile.close();
-  cout << "Writing on file NodeColors.txt completed" << endl;
+}
+
+float kMeans::calcDist(pair<int,int> pCenter, pair<int, int>pNode){
+  float toReturn = pow((pCenter.first-pNode.first),2) + pow((pCenter.second-pNode.second),2);
+  return sqrt(toReturn);
+}
+
+void kMeans::paintCenter(){
+  for (int i = 0; i < centerKeys.size(); ++ i){
+    colors[centerKeys[i]] = "black";
+  }
+}
+
+void kMeans::recalcCenter(){
+  map <int, stack<pair<int,int>>>::iterator it;
+  estable = true;
+  for (it = newCenterCalc.begin(); it != newCenterCalc.end(); ++it){
+    stack<pair<int,int>> tmpStack = it->second;
+    int x = 0;
+    int y = 0;
+    int size = tmpStack.size();
+    while (!tmpStack.empty()){
+      pair<int,int> pairTmp = tmpStack.top(); tmpStack.pop();
+      x += pairTmp.first;
+      y += pairTmp.second;
+    }
+    cout << "old center: " << centers[it->first].first << ", " <<centers[it->first].second << "\t";
+    cout << "new center: " << x/size << ", " << y/size << "\t";
+    float dist = calcDist(centers[it->first], make_pair(x/size,y/size));
+    cout << "dist: " << dist << endl;
+    if(dist< 1.0 and estable) estable = true;
+    else estable = false;
+    centers[it->first] = make_pair(x/size,y/size);
+  }
 }
--- code/kMeans.h
+++ code/kMeans.h
@@ -6,21 +6,27 @@
 #include <random>
 #include <fstream>
 #include <sstream>
+#include <cmath>
 #include "member.h"
 using namespace std;
 
 class kMeans{
   protected:
+    bool estable;
     map <string, string> colors;
     vector<string> mapKeys;
     vector<string> colCell {"b", "g", "r", "c", "m", "y", "purple", "orange", "salmon", "maroon"};
     map <int, pair<int, int>> centers;
     map <int, stack<pair<int,int>>> newCenterCalc;
+    vector<string> centerKeys;
   public:
     void calcKMeans(map<string,Member*> membersInfo, int k);
     void printColorMatrix();
     void calcFirstCenters(map<string,Member*> membersInfo, int k);
-    void printColors();
+    void findNearestCenter(map<string,Member*> membersInfo);
+    void recalcCenter();
+    void paintCenter();
+    float calcDist(pair<int,int> pCenter, pair<int, int>pNode);
 };
 
 #endif
